eaf9a58378cacc1f1d2b327447531fc0e8a64f63
Maxwell Schrader <mcschrader@crimson.ua.edu>
2020-08-23 19:52:02 -0500

    Server fix

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ from dashboard.apply_callbacks import apply_callback
 SESSION = None
 #
 # if
+if not SESSION:
+    SESSION = get_session(config, update_structure=False)
+apply_callback(SESSION)
+app.layout = LAYOUT
+server = app.server
 
 if __name__ == "__main__":
 
-    if not SESSION:
-        SESSION = get_session(config, update_structure=False)
-    apply_callback(SESSION)
-    app.layout = LAYOUT
+    # if not SESSION:
+    #     SESSION = get_session(config, update_structure=False)
+    # apply_callback(SESSION)
+    # app.layout = LAYOUT
     app.run_server(debug=True)
 
